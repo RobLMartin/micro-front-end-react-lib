15a297c4879bc52fe204334d77bfd1157d9535f6
Rob Martin <rmartin3@alumni.unca.edu>
2020-12-20 14:02:09 -0800

    initial commit

---
+++ dist/index.cjs.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var React = require('react');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+
+function MicroFrontend(_ref) {
+  var name = _ref.name,
+      host = _ref.host;
+  React.useEffect(function () {
+    var scriptId = "micro-frontend-script-".concat(name);
+
+    var renderMicroFrontend = function renderMicroFrontend() {
+      window["render".concat(name)]("".concat(name, "-container"));
+    };
+
+    if (document.getElementById(scriptId)) {
+      renderMicroFrontend();
+      return;
+    }
+
+    fetch("".concat(host, "/asset-manifest.json")).then(function (res) {
+      return res.json();
+    }).then(function (manifest) {
+      var script = document.createElement("script");
+      script.id = scriptId;
+      script.crossOrigin = "";
+      script.src = "".concat(host).concat(manifest.files["main.js"]);
+
+      script.onload = function () {
+        renderMicroFrontend();
+      };
+
+      document.head.appendChild(script);
+    });
+    return function () {
+      window["unmount".concat(name)] && window["unmount".concat(name)]("".concat(name, "-container"));
+    };
+  }, []);
+  return /*#__PURE__*/React__default['default'].createElement("main", {
+    id: "".concat(name, "-container")
+  });
+}
+
+MicroFrontend.defaultProps = {
+  document: document,
+  window: window
+};
+
+module.exports = MicroFrontend;
